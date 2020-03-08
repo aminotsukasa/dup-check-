89250c795a850cd556bc7fd27d2b55435e69b2f0
Tomita <tomishuh@f018982822f2.ant.amazon.com>
2020-03-08 12:12:50 +0900

    first commit

---
+++ dup-check.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import hashlib
+from functools import partial
+
+def calchash(file):
+    ha = hashlib.sha1()
+    with open(file,'rb') as f:
+        for buf in iter(partial(f.read, 1024*1024), b''):
+            ha.update(buf)
+    return ha.hexdigest()
+
+
+args=sys.argv
+
+dir1=args[1]
+print("basedir:"+dir1)
+
+htable = dict()
+
+for curdir , dirs, files  in os.walk(dir1):
+    for file in files:
+        fpath =os.path.join(curdir,file)
+        fhash = calchash(fpath)
+        print("x:"+ fpath)
+        if fhash in htable:
+            print( fpath)
+            continue
+        htable[fhash] =  fpath
